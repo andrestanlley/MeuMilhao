44c39a37c598693a5e593021c69d90a635c67079
André Stanlley <stanlley41@gmail.com>
2021-07-20 00:20:33 -0300

    Resolução de bugs e criação de funções

--- funcoes.js
+++ funcoes.js
@@ -3,6 +3,9 @@ var porcentagem = document.getElementById('investimento')
 porcentagem.value = "20"
 var valor = document.getElementById('valor')
 valor.innerText = porcentagem.value
+var juros = document.getElementById('juros')
+var backuprenda = 0
+
 
 
 function valorrenda(){ // Insere o R$ e retorna o valor
@@ -15,7 +18,7 @@ function porcentagem2(){
     valor.innerText = porcentagem.value
     var corresponde = document.getElementById('corresponde')
     var rendaformatado = renda.value.replace('R$ ','').replace(',','.')
-    var backuprenda = rendaformatado
+    backuprenda = rendaformatado
     rendaformatado = Number(rendaformatado*porcentagem.value/100).toFixed()
     if (rendaformatado == 'NaN') {
         var tamanho = backuprenda.length
@@ -23,4 +26,17 @@ function porcentagem2(){
     } else {
         corresponde.innerText = `R$ ${rendaformatado} - ${porcentagem.value}%`
     }
+}
+
+function calcular(){
+    var fortuna = 0
+    var meses = 0
+    juros = juros.value/12
+    var valormensal = Number((backuprenda*porcentagem.value/100).toFixed())
+    while (fortuna <= 1000000){
+        fortuna += fortuna*juros/100
+        fortuna += valormensal
+        meses += 1
+    }
+    alert(`São necessarios ${(meses/12).toFixed()} anos para se obter ${fortuna}`)
 }
